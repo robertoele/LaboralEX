b40e5c1c5e8ddd234632cdc44d2561e08d6c3a58
robertilintilin <47863210+robertilintilin@users.noreply.github.com>
2024-12-17 15:45:36 +0000

    Añadido Scaffold para el formulario de empresas

--- LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/company/InsertCompaniesScreen.kt
+++ LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/company/InsertCompaniesScreen.kt
@@ -1,10 +1,11 @@
 package com.example.laboralex.ui.screens.company
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -13,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -24,26 +26,36 @@ import com.example.laboralex.viewmodel.CompanyViewModel
 
 @Composable
 fun InsertCompaniesScreen(navController: NavController, companyViewModel: CompanyViewModel) {
-    val name = companyViewModel.name.collectAsStateWithLifecycle()
-    var speciality by remember { mutableStateOf("") }
     when (companyViewModel.loadingState.collectAsState().value) {
         State.LOADING -> LoadingScreen()
         else -> {
-            Column {
-                Text("Ahora, añadamos algunas empresas en las que estés interesado")
-                Spacer(Modifier.height(10.dp))
-                Text("Nombre")
-                TextField(name.value, companyViewModel::onNameChanged)
-                Text("Especialidades que busca la empresa: ")
-                DropdownTextField(
-                    elements = companyViewModel.specialities.map { it.name },
-                    value = speciality,
-                    onValueChanged = { speciality = it }
-                ) { }
-                Row {
-                    Button(onClick = companyViewModel::save) { Text("Agregar empresa") }
-                }
+            Scaffold(floatingActionButton = {
+                Button(onClick = {}) { Text("Continuar") }
+            }) {
+                CompaniesForm(modifier = Modifier.padding(it), companyViewModel)
             }
         }
+    }
+}
+
+@Composable
+private fun CompaniesForm(modifier: Modifier = Modifier, companyViewModel: CompanyViewModel) {
+    val name = companyViewModel.name.collectAsStateWithLifecycle()
+    var speciality by remember { mutableStateOf("") }
+    Column(modifier = modifier) {
+        Text("Ahora, añadamos algunas empresas en las que estés interesado")
+        Spacer(Modifier.height(10.dp))
+        Text("Nombre")
+        TextField(name.value, companyViewModel::onNameChanged)
+        Text("Especialidades que busca la empresa: ")
+        DropdownTextField(
+            elements = companyViewModel.specialities.map { it.name },
+            value = speciality,
+            onValueChanged = { speciality = it }
+        ) { }
+        Button(
+            modifier = Modifier.align(Alignment.End),
+            onClick = companyViewModel::save
+        ) { Text("Agregar empresa") }
     }
 }
