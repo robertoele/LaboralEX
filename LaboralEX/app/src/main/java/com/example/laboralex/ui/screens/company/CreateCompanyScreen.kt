720a45c99982c78b7696cb360215f70eab2e7cb4
robertilintilin <47863210+robertilintilin@users.noreply.github.com>
2024-12-18 14:36:05 +0000

    CreateCompany ahora es un Scaffold

--- LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/company/CreateCompanyScreen.kt
+++ LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/company/CreateCompanyScreen.kt
@@ -1,8 +1,10 @@
 package com.example.laboralex.ui.screens.company
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -10,7 +12,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
@@ -25,27 +26,31 @@ fun CreateCompanyScreen(
 ) {
     val name = companyViewModel.name.collectAsStateWithLifecycle()
     var speciality by remember { mutableStateOf("") }
-    Column {
-        Text("Nombre")
-        TextField(name.value, companyViewModel::onNameChanged)
-        Text("Aptitudes que busca la empresa: ")
-        Card {
-            DropdownTextField(
-                elements = companyViewModel.possibleSpecialities.map { it.name },
-                value = speciality,
-                onValueChanged = { speciality = it }
-            ) {
+    Scaffold(
+        floatingActionButton = {
+            Button(
+                onClick = {}
+            ) { Text("Agregar empresa") }
+        }
+    ) { padding ->
+        Column(modifier = Modifier.padding(padding)) {
+            Text("Nombre")
+            TextField(name.value, companyViewModel::onNameChanged)
+            Text("Aptitudes que busca la empresa: ")
+            Card {
+                DropdownTextField(
+                    elements = companyViewModel.possibleSpecialities.map { it.name },
+                    value = speciality,
+                    onValueChanged = { speciality = it }
+                ) {
 
-            }
-            if (companyViewModel.possibleSpecialities.isNotEmpty()) {
-                Text("Sugerencias")
-                ChipFlowRow(companyViewModel.possibleSpecialities.map { it.name })
+                }
+                if (companyViewModel.possibleSpecialities.isNotEmpty()) {
+                    Text("Sugerencias")
+                    ChipFlowRow(companyViewModel.possibleSpecialities.map { it.name })
+                }
             }
         }
-
-        Button(
-            modifier = Modifier.align(Alignment.End),
-            onClick = {}
-        ) { Text("Agregar empresa") }
     }
+
 }
