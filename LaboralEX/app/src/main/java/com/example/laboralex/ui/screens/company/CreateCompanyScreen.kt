ffb4754a6bb76a0e81c12c45e595873cdeeab92f
robertilintilin <47863210+robertilintilin@users.noreply.github.com>
2025-06-20 13:15:57 +0100

    Mejorillas

--- LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/company/CreateCompanyScreen.kt
+++ LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/company/CreateCompanyScreen.kt
@@ -1,5 +1,6 @@
 package com.example.laboralex.ui.screens.company
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Column
@@ -7,8 +8,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
@@ -65,8 +68,15 @@ fun CreateCompanyScreen(
             ) { Text("Agregar empresa") }
         }
     ) { padding ->
-        Column(modifier = Modifier.padding(padding)) {
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .clip(shape = RoundedCornerShape(5.dp))
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.secondaryContainer)
+        ) {
             FormTextField(
+                modifier = Modifier.padding(6.dp),
                 value = name.value,
                 onValueChange = companyViewModel::changeName,
                 onClearPressed = companyViewModel::clearName,
@@ -77,22 +87,23 @@ fun CreateCompanyScreen(
             Spacer(modifier = Modifier.height(3.dp))
 
             Text("Aptitudes que busca la empresa: ")
-            Card {
-                TextFieldWithButton(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = skillId,
-                    onValueChanged = { skillId = it }
-                ) {
+            TextFieldWithButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp),
+                value = skillId,
+                onValueChanged = { skillId = it }
+            ) {
+                companyViewModel.companySkills.add(it)
+                skillId = ""
+            }
+
+            ChipFlowRow(companyViewModel.companySkills)
+
+            if (allSkills.value.isNotEmpty()) {
+                Text("Sugerencias")
+                ChipFlowRow(allSkills.value.map { it.name }) {
                     companyViewModel.companySkills.add(it)
-                }
-
-                ChipFlowRow(companyViewModel.companySkills)
-
-                if (allSkills.value.isNotEmpty()) {
-                    Text("Sugerencias")
-                    ChipFlowRow(allSkills.value.map { it.name }) {
-                        companyViewModel.companySkills.add(it)
-                    }
                 }
             }
         }
