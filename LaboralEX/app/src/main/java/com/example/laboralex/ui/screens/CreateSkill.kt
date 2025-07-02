2a746a52afde03407936840e4187139bba9ff0b7
robertilintilin <47863210+robertilintilin@users.noreply.github.com>
2025-07-02 09:43:12 +0100

    Update CreateSkill.kt

--- LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/CreateSkill.kt
+++ LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/CreateSkill.kt
@@ -4,13 +4,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.laboralex.viewmodel.CreateSkillViewModel
 
@@ -24,14 +25,22 @@ fun CreateSkill(viewModel: CreateSkillViewModel, backNavigation: () -> Unit) {
         ) {
             Text("¿Qué habilidad quieres añadir?")
             TextField(value = skillField.value, onValueChange = viewModel::changeSkillField)
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Button(modifier = Modifier.weight(0.5f), onClick = backNavigation) {
+            Row {
+                Button(
+                    modifier = Modifier
+                        .padding(3.dp),
+                    onClick = backNavigation
+                ) {
                     Text("Cancelar")
                 }
-                Button(modifier = Modifier.weight(0.5f), onClick = {
-                    viewModel.addSkill()
-                    backNavigation()
-                }) {
+                Button(modifier = Modifier
+                    .padding(3.dp),
+                    onClick = {
+                        viewModel.addSkill()
+                        viewModel.changeSkillField("")
+                        backNavigation()
+                    }
+                ) {
                     Text("Añadir")
                 }
             }
