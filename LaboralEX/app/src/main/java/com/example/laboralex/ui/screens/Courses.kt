456372c43375c43c60cb691c5b025d0ef10179b3
robertilintilin <47863210+robertilintilin@users.noreply.github.com>
2025-07-03 17:37:16 +0100

    Update Courses.kt

--- LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/Courses.kt
+++ LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/Courses.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -71,7 +72,10 @@ fun CoursesScreen(viewModel: CoursesViewModel, onCreatePressed: () -> Unit) {
 
 @Composable
 private fun CourseCard(course: CourseWithSkills) {
-    Card(modifier = Modifier.fillMaxWidth()) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
+    ) {
         Column {
             Text(course.course.name)
             ChipFlowRow(course.skills)
