dc667e3b376560cf3b43121c5e958665ffa3bcb0
robertilintilin <47863210+robertilintilin@users.noreply.github.com>
2025-07-05 18:05:22 +0100

    Lista de cursos sin terminar en la pantalla principal

--- LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/main/MainScreen.kt
+++ LaboralEX/app/src/main/java/com/example/laboralex/ui/screens/main/MainScreen.kt
@@ -3,8 +3,10 @@ package com.example.laboralex.ui.screens.main
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,14 +16,17 @@ import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.laboralex.database.entity.company.Company
+import com.example.laboralex.database.entity.course.Course
 import com.example.laboralex.viewmodel.MainScreenViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: MainScreenViewModel) {
-    val userFlow = viewModel.userFlow.collectAsStateWithLifecycle()
+    val users = viewModel.userFlow.collectAsStateWithLifecycle()
+    val unfinishedCourses = viewModel.unfinishedCoursesFlow.collectAsStateWithLifecycle()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -31,7 +36,7 @@ fun MainScreen(viewModel: MainScreenViewModel) {
                 ),
                 title = {
                     Text(
-                        text = "Hola, ${userFlow.value.firstOrNull()?.firstName}",
+                        text = "Hola, ${users.value.firstOrNull()?.firstName}",
                         fontWeight = FontWeight.Bold,
                         style = MaterialTheme.typography.headlineSmall
                     )
@@ -44,14 +49,27 @@ fun MainScreen(viewModel: MainScreenViewModel) {
                 .background(MaterialTheme.colorScheme.surface)
                 .fillMaxSize()
                 .padding(innerPadding)
+                .padding(horizontal = 5.dp)
         ) {
+            if (unfinishedCourses.value.isNotEmpty()) {
+                Text("¡Continúa estos cursos!")
+                unfinishedCourses.value.forEach { CourseCard(it) }
+            }
         }
     }
 }
 
 @Composable
-private fun CompanyCard(company: Company) {
-    Card(onClick = { }) {
-        Text(company.name)
+private fun CourseCard(course: Course) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 5.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
+    ) {
+        Column {
+            Text(course.name, modifier = Modifier.padding(horizontal = 3.dp))
+            Text(course.reference ?: "", modifier = Modifier.padding(horizontal = 3.dp))
+        }
     }
 }
--- LaboralEX/app/src/main/java/com/example/laboralex/viewmodel/MainScreenViewModel.kt
+++ LaboralEX/app/src/main/java/com/example/laboralex/viewmodel/MainScreenViewModel.kt
@@ -2,20 +2,23 @@ package com.example.laboralex.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.laboralex.database.dao.CompanyDao
+import com.example.laboralex.database.dao.CourseDao
 import com.example.laboralex.database.dao.UserDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
 
 @HiltViewModel
 class MainScreenViewModel @Inject constructor(
-    companyDao: CompanyDao,
-    userDao: UserDao
+    userDao: UserDao,
+    courseDao: CourseDao
 ) : ViewModel() {
-    val allCompanies = companyDao.getCompaniesAsFlow()
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
     val userFlow = userDao.getAllAsFlow()
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+    val unfinishedCoursesFlow =
+        courseDao.getAsFlow().transform { list ->
+            emit(list.filter { !it.finished })
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
 }
